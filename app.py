f7fd95f060c93f267ecfb470fc8d0a9e868657f1
woh5152 <whernandez2147@gmail.com>
2025-07-24 06:30:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import requests
 import os
 
-
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Replace for production
 
